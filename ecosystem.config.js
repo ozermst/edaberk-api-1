24cef8f0aab8c3b6cb2eadded8da41ea976a80e5
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2022-03-28 10:41:28 +0300

    PM2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "edaberk-api-1",
       script: "yarn",
       args: "start",
-      watch: ".",
       env_development: {
         NODE_ENV: "development",
         ENV_PATH: ".env.development",
@@ -12,6 +11,10 @@ module.exports = {
       env_test: {
         NODE_ENV: "test",
         ENV_PATH: ".env.test",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        ENV_PATH: ".env.production",
       },
     },
   ],
