f7734604085653bc90532be920100dd46e8f883a
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2022-04-06 18:39:54 +0300

    new api resources added

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  apiToken: {
-    salt: env("API_TOKEN_SALT", "someRandomLongString"),
-  },
   auth: {
     secret: env("ADMIN_JWT_SECRET", "someRandomLongString"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "someRandomLongString"),
   },
 });
