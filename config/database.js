ac8ed9bd7aa4d3d9eaded9b0e540f285a01958c9
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2022-04-06 21:55:19 +0300

    Strapi upgrade-3

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
+const path = require("path");
+
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: "sqlite",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", ""),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", false),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
+    useNullAsDefault: true,
   },
 });
